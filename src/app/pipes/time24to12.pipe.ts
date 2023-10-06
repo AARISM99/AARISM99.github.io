d913971e46e7dbe2dfd7eebbbb3d2a46a8e2172b
AARABSIM99 <ismael.aarab@etu.uae.ac.ma>
2023-10-06 10:33:28 +0100

    project init

--- src/app/pipes/time24to12.pipe.ts
+++ src/app/pipes/time24to12.pipe.ts
@@ -1,4 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { GlobalConstants } from '../global/globalConstants';
 
 
 @Pipe({ name: 'time24to12Transform' })
@@ -6,8 +7,14 @@ export class Time24to12Format implements PipeTransform {
   transform(time: any): any {
 
   var time24To12 = function(a: any) {
+    var constants: GlobalConstants = new GlobalConstants();
       //below date doesn't matter.
-    return (new Date("1955-11-05T" + a + "Z")).toLocaleTimeString("bestfit", {
+    return constants._lang == "ar" ? (new Date("1955-11-05T" + a + "Z")).toLocaleTimeString("bestfit", {
+      timeZone: "UTC",
+      hour12: !0,
+      hour: "numeric",
+      minute: "numeric"
+    }).replace("AM","صباحا").replace("PM","مساء") : (new Date("1955-11-05T" + a + "Z")).toLocaleTimeString("bestfit", {
       timeZone: "UTC",
       hour12: !0,
       hour: "numeric",
